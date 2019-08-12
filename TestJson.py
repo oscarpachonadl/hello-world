1ba6e25cf92807fb992e4fc22d53378c699490b6
oscarpachonadl <53241056+oscarpachonadl@users.noreply.github.com>
2019-08-12 11:15:54 -0500

    Fix error in read of files

--- TestJson.py
+++ TestJson.py
@@ -16,7 +16,7 @@ def list_bucket_objects_s3(bucket_name_list):
             print('   + Object: ', o.key)
 
 def fix_json_str(str_data):
-    str_data = str_data.strip()
+    str_data = str_data.decode().strip()
     if str_data[-1] == ',':
         str_data = '[' + str_data[:-1] + ']'
     return json.loads(str_data)
@@ -33,26 +33,33 @@ def count_json_elements(file_name):
 
 
 def count_json_elements_bucket(bucket_name):    
+    print('-- Bucket:', bucket_name)
     bucket_objects = get_objects_s3(bucket_name)
+    count = 0
     for bo in bucket_objects:
         try:
             s3 = boto3.client('s3')
             o = s3.get_object(Bucket=bo.bucket_name, Key=bo.key)
             json_data = fix_json_str(o['Body'].read())
-            print('    + Count: ', len(list(json_data)), ' Object: ', bo.key)
+            count_o = len(list(json_data))
+            print('    + Count: ', count_o, ' Object: ', bo.key)
+            count += count_o
         except Exception as e:
             print('Error in Bucket:', bo.bucket_name, 'Object:', bo.key, '\n    Error:', e)
+    return count
 
 
-            
-# count_json_elements("G:\\Mi unidad\\Digital Analytics\\ArchivosDescargados\\adl-analytics-bb-occiauto-firehose-1-2019-08-01-21-13-08-2f4c12b2-dcde-4fde-a8b8-35bdcc2f3ef8")
-# count_json_elements("G:\\Mi unidad\\Digital Analytics\\ArchivosDescargados\\adl-analytics-bb-occiauto-firehose-1-2019-08-01-21-07-57-327b0e21-0f19-446d-be1f-c8dcc32e1216")
+def count_json_elements_buckets(bucket_name_list):
+    total_count = 0
+    for b in bucket_name_list:
+        total_count += count_json_elements_bucket(b)
+    print('** Total Registros:', total_count)
 
-# list_bucket_objects_s3(
-#         ['adl-analytics-bb-occiauto', 
-#         'adl-analytics-rb-pb', 
-#         'adl-analytics-bb-pb', 
-#         'adl-analytics-gb-pb', 
-#         'adl-analytics-yb-pb'])
 
-count_json_elements_bucket('adl-analytics-bb-occiauto')
+#Ejecución
+count_json_elements_buckets(
+            ['adl-analytics-bb-occiauto', 
+            'adl-analytics-rb-pb', 
+            'adl-analytics-bb-pb', 
+            'adl-analytics-gb-pb', 
+            'adl-analytics-yb-pb'])
